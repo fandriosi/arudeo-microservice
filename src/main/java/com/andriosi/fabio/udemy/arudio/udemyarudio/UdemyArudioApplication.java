1194a7b628f2c941c033e14ada65d6f1e9e6893c
fandriosi <fabio.andriosi@gmail.com>
2023-02-20 10:15:05 -0300

    First commit

---
+++ src/main/java/com/andriosi/fabio/udemy/arudio/udemyarudio/UdemyArudioApplication.java
@@ -0,0 +1,13 @@
+package com.andriosi.fabio.udemy.arudio.udemyarudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyArudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyArudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andriosi/fabio/udemy/arudio/udemyarudio/UdemyArudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andriosi.fabio.udemy.arudio.udemyarudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyArudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
